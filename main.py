3a7279d660781850e7a8efb487ac6bd594395115
amanvaidya <amanvaidya700@gmail.com>
2025-02-20 22:51:33 +0530

    added sqllite3 db connect and insert script for few unit test cases. added exit condition for the prompt

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from prompt.prompt_handler import get_greeting_response
+from db.database import create_database
+from db.insert_data import insert_sample_data
+
+# Ensure database and initial data are set up before running the app
+create_database()
+insert_sample_data()
 
 # Get user input for the prompt
-user_prompt = input("Enter your prompt: ")
-response = get_greeting_response(user_prompt)
-print(response)
+flag = True
+while flag:
+    user_prompt = input("Enter your prompt: ")
+    if user_prompt.lower() == "exit":
+        flag = False
+        break
+    response = get_greeting_response(user_prompt)
+    print(response)
